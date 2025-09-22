4a7a403d602487425448a623766bd3830a507acc
Olivier Carignan <olive.carignan@gmail.com>
2025-09-22 12:44:35 -0400

    resources page

--- src/app/resources/page.jsx
+++ src/app/resources/page.jsx
@@ -10,7 +10,32 @@ export default async function ResourcesPage() {
   return (
     <main className="about-page page subgrid">
       <div className="sidebar">
-
+        <div className="list">
+          <h1 className="body-text">Los Angeles Literature: Resources</h1>
+          <ul>
+            {resourcePage.resourcesCollection.items.map((resource) => (
+              <li key={resource.name} className="resource-item">
+                <a href={resource.url} className="body-text" target="_blank" rel="noreferrer">
+                  <svg
+                    width="13"
+                    height="13"
+                    viewBox="0 0 13 13"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2.18507 12.4541L0.963135 11.2338L10.0493 2.1438H3.03102L3.04669 0.454102H12.9631V10.3733H11.2556L11.2712 3.36414L2.18507 12.4541Z"
+                      fill="#2E2E2E"
+                    />
+                  </svg>
+                  <h2 className="body-text">
+                    {resource.name}
+                  </h2>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="main-content">
         <Markdown>{resourcePage.pageContent}</Markdown>
