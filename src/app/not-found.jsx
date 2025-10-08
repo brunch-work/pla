9719c8dc988c40f04716a26c4628ed7ffc11755b
Olivier Carignan <olive.carignan@gmail.com>
2025-10-08 16:24:16 -0400

    fix build error

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,20 +1,33 @@
 "use client";
 
 import { NavContextProvider } from "@/utils/navContextProvider";
+import { Suspense } from "react";
 import { Nav } from "@/components/Nav";
 import { useLoader } from "@/utils/loader";
+import { Logo } from "@/components/Logo";
 
 import { useEffect } from "react";
 
-export default function NotFound() {
+const navFallback = (
+  <nav className="nav grid" aria-labelledby="main navigation">
+    <div className="subgrid">
+      <div className="logo-wrapper">
+        <Logo />
+      </div>
+    </div>
+  </nav>
+);
 
+export default function NotFound() {
   useEffect(() => {
     useLoader.setState({ showLoader: false, isDone: true });
   }, []);
 
   return (
     <NavContextProvider>
-      <Nav />
+      <Suspense fallback={navFallback}>
+        <Nav />
+      </Suspense>
       <div className="not-found page subgrid">
         <h1 className="body-text">404 - Page Not Found</h1>
       </div>
